4bed5cffeb91264199d1b5eaec94f8ea9b54641d
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-18 11:19:17 -0400

    don't know how to use gatsby pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,32 @@
-const path = require('path')
+// const path = require('path')
 
-exports.createPages = ({graphql, actions}) => {
-  const { createPage } = actions
-  const Portfolio = path.resolve('src/components/portfolio.js')
 
-  return gaphql(`
-  query loadPagesQuery ($limit: Int!) {
-    allMarkdownRemark(limit: $limit){
-      edges{
-        node{
-          frontmatter {
-            slug
-          }
-        }
-      }
-    }
-  }
-  `,{ limit: 1000 }).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-  })
-}
+// exports.createPages = async ({graphql, actions}) => {
+//   const { createPage } = actions
+//   const Portfolio = path.resolve('src/components/portfolio.js')
 
-result.data.allMarkdownRemark.eduges.forEach(edge => {
-  createPage({
-    path: `${edge.node.frontmatter.slug}`,
-    component: Portfolio,
-  })
-})
+//   return gaphql(`
+//   query loadPagesQuery ($limit: Int!) {
+//     allMarkdownRemark(limit: $limit){
+//       edges{
+//         node{
+//           frontmatter {
+//             slug
+//           }d
+//         }
+//       }
+//     }
+//   }
+//   `,{ limit: 1000 }).then(result => {
+//     if (result.errors) {
+//       throw result.errors
+//     }
+//     result.data.allMarkdownRemark.eduges.forEach(edge => {
+//       createPage({
+//         path: `${edge.node.frontmatter.slug}`,
+//         component: Portfolio, 
+//       })
+//     })
+//   })
+// }
+
