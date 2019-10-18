b64190962f4244c9f21db145dd0b9bb64aec56cd
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-18 10:48:53 -0400

    gatsby page

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,5 +2,29 @@ const path = require('path')
 
 exports.createPages = ({graphql, actions}) => {
   const { createPage } = actions
-  const 
+  const Portfolio = path.resolve('src/pages/portfolio.js')
+
+  return gaphql(`
+  query loadPagesQuery ($limit: Int!) {
+    allMarkdownRemark(limit: $limit){
+      edges{
+        node{
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  }
+  `,{ limit: 1000 }).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+  })
 }
+
+result.data.allMarkdownRemark.eduges.forEach(edge => {
+  createPage({
+    path: $
+  })
+})
