737c0448f9060cf6d8269767f149761ae4403c70
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-27 16:20:31 -0400

    meh

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React, {Component} from "react"
+import styled from 'styled-components'
+import Landing from "../components/landing"
+import './fonts.css'
 
+document.body.style.margin = 0
 
 class IndexPage extends Component {
   render(){
     return(
       <>
+      <Landing />
+       
       </>
     )
   }
