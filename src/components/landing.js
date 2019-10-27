29c94994814bae63a62ec5e4963a05ee103e5550
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-27 15:44:25 -0400

    whack tree

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,35 +1,67 @@
 import React, { Component } from "react"
 import styled from 'styled-components'
 import "./landing.css"
+import {Link} from 'gatsby'
+import bgImg from '../images/bg.jpg'
+import { NONAME } from "dns"
 
 
-const Description = styled.h1`
-color: #a6a6a6;
+const Description = styled.ul`
+  color: #a6a6a6;
+  font-size: 85px;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 300;
+`
+
+const LandingContainer = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  background-image: linear-gradient(
+    to bottom, 
+    rgba(0, 0, 0, 1),
+    rgba(79, 79, 79, 0.49)),
+    url(${bgImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  align-content: center;
+  justify-content:center;
+`
+
+const CleanListItem = styled.li`
+  list-style: none;
+`
+
+const Name = styled.h1`
+color:white;
+padding:20px 15px;
 font-size: 85px;
-position: relative;
-top:15px;
-left: 635px;
+border-right: 2px solid #8f0000;
+height: 15%;
 font-family: 'Josefin Sans', sans-serif;
 font-weight: 300;
+display: flex;
+flex-direction: row;
+list-style:none;
+&:hover{color:#8f0000}
 `
+
 
 class Landing extends Component {
   render() {
     return (
-      <>
-        <h1 className='name'>
-          YOUSEF
-      </h1>
-        <Description>
-          <ul>
-          <li>AHMAD</li>
-          <li>CREATIVITY</li>
-          <li>ENGINEERING</li>
-          <li>SOFTWARE</li>
-          <li>STRATEGY</li>
-          </ul>
-      </Description>
-      </>
+      <LandingContainer>
+          <Name>
+            <Link to='./page-2' style={{textDecoration: 'none'}}>YOUSEF</Link>
+          </Name>
+          <Description>
+            <CleanListItem>AHMAD</CleanListItem>
+            <CleanListItem>CREATIVITY</CleanListItem>
+            <CleanListItem>ENGINEERING</CleanListItem>
+            <CleanListItem>SOFTWARE</CleanListItem>
+            <CleanListItem>STRATEGY</CleanListItem>
+        </Description>
+      </LandingContainer>
 
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import styled from 'styled-components'
 import Landing from "../components/landing"
 import './fonts.css'
 
+document.body.style.margin = 0
+
 class IndexPage extends Component {
   render(){
     return(
       <>
-      <Landing>
+      <Landing />
        
-      </Landing>
       </>
     )
   }
