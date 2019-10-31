60b00896639241d2b6c0bb38febeff130f3f6b2c
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-31 15:58:25 -0400

    this should allow netlify to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React, {Component} from "react"
 import Landing from "../components/landing"
 import './fonts.css'
 
-document.body.style.margin = 0
 
 class IndexPage extends Component {
   render(){
